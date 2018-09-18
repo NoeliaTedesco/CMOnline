e7b90e9e744c351d555d181f8dfe5347722680e2
NoeliaTedesco <33137554+NoeliaTedesco@users.noreply.github.com>
2018-09-18 09:42:05 -0300

    Se suben cambios en la clase Test_Prestadores

--- src/test/java/ar/com/osde/cmonline/selenium/Prestadores.java
+++ src/test/java/ar/com/osde/cmonline/selenium/Prestadores.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Prestadores {
@@ -21,6 +22,122 @@ public class Prestadores {
 	@FindBy(xpath = "//button[@type='submit']")	    
 	private WebElement bEnviar;
 	
-	
+	@FindBy(xpath = ("(.//*[normalize-space(text()) and normalize-space(.)='Gestión de prácticas'])[1]/following::h5[1]"))
+	private WebElement menuGestionPractica;
 
+	@FindBy(css = "button.btn.btn-success")
+	private WebElement btnEnviarInvitacion;
+
+	@FindBy(xpath = ("(.//*[normalize-space(text()) and normalize-space(.)='Enviar Invitacion'])[1]/following::div[3]"))
+	private WebElement formularioInvitacion;
+
+	@FindBy(name = "valorIdentificacion")
+	private WebElement valorIdentificacion;
+	
+	@FindBy(id = "email")
+	private WebElement campoEmail;
+
+	@FindBy(id = "phone")
+	private WebElement campoTelefono;
+	
+	@FindBy (id = "clinica")
+	private WebElement especialidadClinica;
+	
+	@FindBy (id = "pediatria")
+	private WebElement especialidadPedriatrica;
+
+	@FindBy(name = "detalle")
+	private WebElement campoDetalle;
+	
+	@FindBy(id = "apellido")
+	private WebElement campoApellido;
+
+	@FindBy(id = "nombre")
+	private WebElement campoNombre;
+
+	@FindBy(id = "fechaNacimiento")
+	private WebElement campoFechaNacimiento;
+
+	@FindBy(id = "id_servicio")
+	private WebElement campoIdServicio;
+
+	@FindBy (xpath= ("//*[@id=\"enviarInvitacionForm\"]/div/footer/div/div/div/div[2]/button"))
+	private WebElement btnEnviar;
+	
+	@FindBy (css = "i.fa.fa-2x.fa-trash")
+	private WebElement btnAnular;
+	
+	@FindBy (name = "detalleMotivo")
+	private WebElement campoMotivoAnular;
+	
+	@FindBy (xpath = ("//button[2]"))
+	private WebElement btnConfirmar;
+	
+	@FindBy (css ="i.fa.fa-2x.fa-mail-forward")
+	private WebElement btnReenviarInvitacion;
+	
+	public Prestadores(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(this.driver,this);		
+	}
+	
+	public void IniciarSesion(String usuarioPrueba, String contrasena) {
+		try {
+			cUsuario.click();
+			cUsuario.sendKeys(usuarioPrueba);
+			cContrasena.clear();
+			cContrasena.click();
+			cContrasena.sendKeys(contrasena);
+			bEnviar.click();
+		} catch (Exception e) {
+			System.out.println("Error al iniciar sesión");
+		}
+
+	}
+
+	public void IngresarGestionPractica() {
+		try {
+			if (menuGestionPractica.isDisplayed()) {
+				menuGestionPractica.click();
+			} else {
+				System.out.println("No se encuentra visible el botón para ingresar a la gestión de prácticas");
+			}
+		} catch (Exception e) {
+
+		}
+
+	}
+
+	public void AbrirEnviarInvitacion() {
+		btnEnviarInvitacion.click();
+	}
+
+	public boolean esVisibleFormularioInvitacion() {
+		if (formularioInvitacion.isDisplayed()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	
+	public void crearInvitacion(String nroSocio, String celular, String email, String especialidad, String detalle,
+			String nombre, String apellido,  String fechaNacimiento, String idServicio) {
+		
+		valorIdentificacion.sendKeys(nroSocio);
+		campoTelefono.sendKeys(celular);
+		campoEmail.sendKeys(email);
+		switch (especialidad) {
+		case "Clinica" : especialidadClinica.click();
+		case "Pedriatria" : especialidadPedriatrica.click();
+		}
+		campoDetalle.sendKeys(detalle);
+		campoNombre.sendKeys(nombre);
+		campoApellido.sendKeys(apellido);
+		campoFechaNacimiento.sendKeys(fechaNacimiento);
+		campoIdServicio.sendKeys(idServicio);
+		btnEnviar.click();
+	}
+	
+	
 }
--- src/test/java/ar/com/osde/cmonline/testng/Test_Prestadores.java
+++ src/test/java/ar/com/osde/cmonline/testng/Test_Prestadores.java
@@ -2,21 +2,32 @@ package ar.com.osde.cmonline.testng;
 
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
 import ar.com.osde.cmonline.selenium.Base_Test;
+import ar.com.osde.cmonline.selenium.Prestadores;
 	
 
 public class Test_Prestadores {
 	
 	private Base_Test driver;
+	private Prestadores prestador;
 	
 	@Parameters({"rol", "ipNodo"})
 	@BeforeClass
 	public void startUp(String rol, String ipNodo) throws InterruptedException {
 		driver = new Base_Test(rol, ipNodo);
-		driver.ingresarPagina();
-		
-		
+		driver.ingresarPagina();			
+	}
+	
+	@Parameters ({"usuario", "contrasena"})
+	@Test
+	public void invitaciones(String usuario, String contrasena) {
+		prestador = new Prestadores(driver.getDriver());
+		prestador.IniciarSesion(usuario, contrasena);
+		prestador.IngresarGestionPractica();
+		prestador.AbrirEnviarInvitacion();
+		prestador.esVisibleFormularioInvitacion();
 	}
 
 }
